53f945bcac744c749eb8394a3782fe42ee320cd4
originalusrnm <jshornbuckle@gmail.com>
2017-07-23 15:43:39 -0400

    Update exercise

--- Exercise-2_0/src/exercise-2_0.ts
+++ Exercise-2_0/src/exercise-2_0.ts
@@ -6,9 +6,7 @@
 //Note: If your server is not already running then type npm start at the command line from the root.
 /////////////
 
-//1. - Open up a browser and navigate to http://localhost:8000/Exercise-2_0/  
-//   - Compile to typscript from the terminal by cd into directory and running tsc
-//   - Click the button labeled 'varTest' and note that the alert shows a 2.
+//1. - Click the button labeled 'varTest' and note that the alert shows a 2.
 //   - using your knoledge of var and let, Fix this code so that the alert shows the original value of 1.  
 
 function varTest() {
@@ -19,5 +17,31 @@ function varTest() {
   }
 
   alert(x);
-  
+
+}
+
+// 2. PI is getting changed to an invalid number.  Change the code so that the compiler catches this error; Note:
+//    You should check out the compiler messages in your terminal window.  If you are using Visual Studio Code, you will 
+//    see syntax error highlighting when the typescript is failing.
+//    When the compiler identifies the error you should remove it.
+function keepPiConstant() {
+    
+    var PI: number = 3.14;
+    var radius: number = 2;
+
+    PI = 4;
+
+    alert(PI * (radius * radius));
+
+}
+
+// 3.  You want to keep secretName scoped to the if block. Fix the code so that the compiler catches this
+//     error before production.
+function keepItInTheBlock() {
+    
+    if (true) {
+        var secretName = "Secret Revelaed! No!";
+    }
+
+    alert(secretName);
 }
