1b6c052a2bd740a5850f773a724a57da442e6f41
originalusrnm <jshornbuckle@gmail.com>
2017-07-30 08:51:08 -0400

    Add Exercise 4

---
+++ Exercise-4_0/src/exercise-4_0.ts
@@ -0,0 +1,9 @@
+
+//1. Task: Create an Atm class that allows a user to authenticate, logout, check his/her balance, get money, add money 
+//   to their account.  The Atm class should internally use an instance of an Account class and an instance of a Bank
+//   class that stores a list of all accounts. 
+//   
+//   You will use the browser console to test your atm.  You will need to log write console messages using console.log()
+//   console.error();
+//
+//   Instantiate an instance of the atm in the code here so that it is available in your browser console. 
