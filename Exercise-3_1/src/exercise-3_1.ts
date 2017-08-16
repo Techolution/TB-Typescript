a6c13478a8cc8cefa534674502926a8eccbda6e0
originalusrnm <jshornbuckle@gmail.com>
2017-08-16 14:08:16 -0400

    Update

--- Exercise-3_1/src/exercise-3_1.ts
+++ Exercise-3_1/src/exercise-3_1.ts
@@ -6,6 +6,18 @@
 //    Delcare a variable named myCar to be of type ICar and set it to an object where hybrid = true;
 //    Update the ICar interface so that the hybrid property cannot be changed after it is set the first time. 
 //
+interface ICar {
+    color?: string;
+    readonly hybrid: boolean;
+    maxSpeed: number;
+    numberOfDoors: number;
+}
+
+var myCar: ICar = {
+    hybrid: true,
+    maxSpeed: 1000,
+    numberOfDoors: 4
+};
 
 //////////////////
 // 2.  Declare a new interface name IMusician.
@@ -13,6 +25,10 @@
 //     It should have a method called play that takes a song title and returns 
 //     true if the song title is found in its songs array or false if it is not.
 //
+interface IMusician {
+    songs: string[];
+    play(songTitle: string): boolean;
+}
 
 //////////////////
 // 3. Below is a variable employeeHashMap.  It should be a hash with key value pairs where the
@@ -25,13 +41,40 @@ interface Employee {
     lastName: string;
 }
 
-let employeeHashMap;
+let employeeHashMap:  { [ lastName: string]: Employee};
+
+employeeHashMap = {
+    "Johnson": {
+        age: 30,
+        firstName: "Alex",
+        lastName: "Johonson"
+    },
+    "Smith": {
+        age: 34,
+        firstName: "John",
+        lastName: "Smith"
+    },
+    "Dinesh": {
+        age: 20,
+        firstName: "Patel",
+        lastName: "Dinesh"
+    }
+}
 
 //////////////////
 // 4.  Below is a simple variable names x with 3 properties set. Declcare two interfaces: ISquare, with 
 //     a width and height and an ICube which extends ISquare and adds a length property. Set x to be of type ICube.
 
-let x;
+interface ISquare {
+    height: number;
+    width: number;
+}
+
+interface ICube extends ISquare {
+    length: number;
+}
+
+let x: ICube;
 
 x.width = 20;
 x.height = 10;
