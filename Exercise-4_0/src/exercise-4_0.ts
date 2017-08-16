0dff3bbe6125346dd1e9cf4cac02f8a319d4b593
originalusrnm <jshornbuckle@gmail.com>
2017-08-16 14:42:09 -0400

    update

--- Exercise-4_0/src/exercise-4_0.ts
+++ Exercise-4_0/src/exercise-4_0.ts
@@ -1,10 +1,17 @@
 
 //1. Task: Create an Atm class that allows a user to authenticate, logout, check his/her balance, get money, add money 
-//   to their account.  The Atm class should internally use an instance of an Account class and an instance of a Bank
-//   class that stores a list of all accounts. 
-//   
-//   You will use the browser console to test your atm.  You will need to log write console messages using console.log()
-//   console.error();
+//   to their account.  
+//
+//   You should create an Account class and a Bank class.  
+//   The Account class should manage the concerns of the account by knowing the username and password, the current balance, 
+//   and adding/removing funds to or from the balance.
+//
+//   The Bank class should maintain a list of accounts and have one method: getAccount which takes a user name and password
+//   and searches returns an account if found.
+//
+//   Create an account and a bank in your code and instantiate and atm, passing in the bank instance.  Add a public api to 
+//   your Atm class to accomplish the above tasks.  
+//   Use the browser console to test your atm.
 //
 //   Instantiate an instance of the atm in the code here so that it is available in your browser console. 
 
