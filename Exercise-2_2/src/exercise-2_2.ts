4353ea63bc097877e53911c95d128a1aa58d8574
originalusrnm <jshornbuckle@gmail.com>
2017-08-16 13:37:18 -0400

    Update exercise 2-2

--- Exercise-2_2/src/exercise-2_2.ts
+++ Exercise-2_2/src/exercise-2_2.ts
@@ -1,6 +1,7 @@
 
 //1.  Fix messageOne so that the message outputs correctly in the browser.
 function messageOne() {
+    let adjective = "cool";
     let output: any = document.getElementById('message1');
     let message: string = `I am a ${adjective} message`;
 
